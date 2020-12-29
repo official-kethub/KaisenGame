179d2e04812b437b325de9955ba533b2e067b11e
Keisuke-oor <76739533+Keisuke-oor@users.noreply.github.com>
2020-12-29 23:40:44 +0900

    Add files via upload

---
+++ Ex00.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Ex00 {
+  public static void main(String args[]){
+    int n = 10;
+    for (int i = 1; i <= n; i++) {
+       System.out.println(i);
+    }
+  }
+}
