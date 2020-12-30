495154a9158c55d1e4ace9a9dbf2c3f97f49bbd9
Keisuke-oor <Keisuke-oor@users.noreply.github.com>
2020-12-30 21:09:46 +0900

    Create kaisen_3.java
    俺の担当分です

---
+++ kaisen_3.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class kaisen_3 {
+
+  public static void main(String[] args) {
+    int i, k, l;
+    int c1, c2, c3, c4;
+    Scanner sc = new Scanner(System.in);
+    c1 = c2 = c3 = c4 = 0;
+
+    do {
+      System.out.println("Please type in 'i'");
+      i = sc.nextInt();
+      if (i == 1) {
+        k = 1;
+        c2 = c3 = c4 = 0;
+        c1++;
+        System.out.println("c1: " + c1 + " c2: " + c2 + " c3: " + c3 + " c4: " + c4);
+      }
+      if (i == 2) {
+        k = 2;
+        c1 = c3 = c4 = 0;
+        c2++;
+        System.out.println("c1: " + c1 + " c2: " + c2 + " c3: " + c3 + " c4: " + c4);
+      }
+      if (i == 3) {
+        k = 3;
+        c2 = c1 = c4 = 0;
+        c3++;
+        System.out.println("c1: " + c1 + " c2: " + c2 + " c3: " + c3 + " c4: " + c4);
+      }
+      if (i == 4) {
+        k = 4;
+        c2 = c3 = c1 = 0;
+        c4++;
+        System.out.println("c1: " + c1 + " c2: " + c2 + " c3: " + c3 + " c4: " + c4);
+      }
+      i = 0;
+    } while (true);
+
+  }
+}
