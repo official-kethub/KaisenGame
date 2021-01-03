d0d84f36040bedb9f29174214be5c82edd60c6a3
official-kethub <76738596+official-kethub@users.noreply.github.com>
2021-01-04 01:29:16 +0900

    Create hensu.java

---
+++ hensu.java
@@ -0,0 +1,11 @@
+public class hensu{
+    public static int c[] = new int[5];
+    public static int [] hp = {5, 5, 5, 5};
+    public static int j = 0;
+    public static int attac  = 0;
+    public static int k, l;
+    public static int zenkai = 99;
+    public static int hozk = 99;
+    public static int hozl = 99;
+    public static int attackcnt = 0;
+}
